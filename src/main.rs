a3158182e0848611d20e6581a6b1a93fb20195e2
niels <highintothesky@github.com>
2019-08-03 18:29:17 +0200

    fail #2

--- src/main.rs
+++ src/main.rs
@@ -1,75 +1,106 @@
 extern crate piston_window;
-extern crate find_folder;
-extern crate tiled;
 
 use piston_window::*;
-use std::fs::File;
-use tiled::parse;
 
-fn main() {
-    let assets = find_folder::Search::ParentsThenKids(3, 3)
-        .for_folder("assets")
-        .unwrap();
+struct Game {
+    rotation: f64,
+    x: f64,
+    y: f64,
+    up_d: bool, down_d: bool, left_d: bool, right_d: bool
+}
 
-    let file = File::open(assets.join("tiled_base64_zlib.tmx")).unwrap();
-    let map = parse(file).unwrap();
-
-    let opengl = OpenGL::V3_2;
-    let mut window: PistonWindow = WindowSettings::new("piston: tiled", [600, 600])
-        .exit_on_esc(true)
-        .opengl(opengl)
-        .build()
-        .unwrap();
-
-    let tileset = map.get_tileset_by_gid(1).unwrap();
-    let tile_width = tileset.tile_width;
-    let tile_height = tileset.tile_height;
-
-    let tilesheet = assets.join(&tileset.images[0].source);
-    let tilesheet = Texture::from_path(
-        &mut window.create_texture_context(),
-        &tilesheet,
-        Flip::None,
-        &TextureSettings::new(),
-    ).unwrap();
-
-    let (width, _) = tilesheet.get_size();
-    let layer: &tiled::Layer = &map.layers[0];
-    let image = Image::new();
-
-    while let Some(e) = window.next() {
+impl Game {
+    fn new() -> Game {
+        Game { rotation : 0.0, x : 0.0, y : 0.0, up_d: false, down_d: false, left_d: false, right_d: false }
+    }
+    fn on_update(&mut self, upd: UpdateArgs) {
+        self.rotation += 3.0 * upd.dt;
+        if self.up_d {
+            self.y += (-50.0) * upd.dt;
+        }
+        if self.down_d {
+            self.y += (50.0) * upd.dt;
+        }
+        if self.left_d {
+            self.x += (-50.0) * upd.dt;
+        }
+        if self.right_d {
+            self.x += (50.0) * upd.dt;
+        }
+    }
+    fn on_draw(&mut self, ren: RenderArgs, e: PistonWindow) {
         e.draw_2d(|c, g| {
-            clear([0.5; 4], g);
-
-            for (y, row) in layer.tiles.iter().enumerate().clone() {
-                for (x, &tile) in row.iter().enumerate() {
-                    if tile == 0 {
-                        continue;
+            clear([0.0, 0.0, 0.0, 1.0], g);
+            let center = c.transform.trans((ren.width / 2) as f64, (ren.height / 2) as f64);
+            let square = rectangle::square(0.0, 0.0, 100.0);
+            let red = [1.0, 0.0, 0.0, 1.0];
+            rectangle(red, square, center.trans(self.x, self.y).rot_rad(self.rotation).trans(-50.0, -50.0), g); // We translate the rectangle slightly so that it's centered; otherwise only the top left corner would be centered
+        });
+    }
+    fn on_input(&mut self, inp: Input) {
+        match inp {
+            Input::Press(but) => {
+                match but {
+                    Button::Keyboard(Key::Up) => {
+                        self.up_d = true;
                     }
-
-                    let tile = tile - 1; // tiled counts from 1
-
-                    // rect of the particular tile in the tilesheet
-                    let src_rect = [
-                        (tile % (width / tile_width) * tile_width) as f64,
-                        (tile / (width / tile_height) * tile_height) as f64,
-                        tile_width as f64,
-                        tile_height as f64,
-                    ];
-
-                    let trans = c.transform.trans(
-                        x as f64 * tile_width as f64,
-                        y as f64 * tile_height as f64,
-                    );
-
-                    image.src_rect(src_rect).draw(
-                        &tilesheet,
-                        &DrawState::default(),
-                        trans,
-                        g,
-                    );
+                    Button::Keyboard(Key::Down) => {
+                        self.down_d = true;
+                    }
+                    Button::Keyboard(Key::Left) => {
+                        self.left_d = true;
+                    }
+                    Button::Keyboard(Key::Right) => {
+                        self.right_d = true;
+                    }
+                    _ => {}
                 }
             }
-        });
+            Input::Release(but) => {
+                match but {
+                    Button::Keyboard(Key::Up) => {
+                        self.up_d = false;
+                    }
+                    Button::Keyboard(Key::Down) => {
+                        self.down_d = false;
+                    }
+                    Button::Keyboard(Key::Left) => {
+                        self.left_d = false;
+                    }
+                    Button::Keyboard(Key::Right) => {
+                        self.right_d = false;
+                    }
+                    _ => {}
+                }
+            }
+            _ => {}
+        }
+    }
+}
+
+fn main() {
+    let window: PistonWindow = WindowSettings::new(
+        "piston-tutorial",
+        [600, 600]
+    )
+    .exit_on_esc(true)
+    .build()
+    .unwrap();
+    let mut game = Game::new();
+    for e in window {
+        match e.event {
+            Some(Event::Update(upd)) => {
+                game.on_update(upd);
+            }
+            Some(Event::Render(ren)) => {
+                game.on_draw(ren, e);
+            }
+            Some(Event::Input(inp)) => {
+                game.on_input(inp);
+            }
+            _ => {
+
+            }
+        }
     }
 }
